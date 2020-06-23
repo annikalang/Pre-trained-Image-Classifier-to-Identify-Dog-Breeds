5c3dc7cf136b872c4ccb49080e2022aacfc530e4
Annika Lang <Lang-annika@t-online.de>
2020-06-23 12:56:44 +0200

    fixed bug to ignore hidden files (in this case, pet images)

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: Annika Lisa Lang
 # DATE CREATED: 19.06.2020                                 
-# REVISED DATE: 
+# REVISED DATE: 23.06.2020
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
 #           - The Image Folder as image_dir within get_pet_labels function and 
@@ -49,6 +49,9 @@ def get_pet_labels(image_dir):
     # create empty list for pet labels
     pet_labels = []
     
+    # create empty dictionary    
+    results_dic = dict()
+    
     # iterate over each file name to save the pet name individually
     for index in range(0, len(filename_list), 1):
         if filename_list[index][0] != ".":
@@ -64,24 +67,20 @@ def get_pet_labels(image_dir):
             pet_name = pet_name.strip()
             print('Filename = ', pet_image_filename, '    label = ', pet_name)
             ######################## THE pet names should be saved in a list or sth. --> pet_labels
-            pet_labels.append(pet_name)
-            
-            
-        print('\n{:2d} file: {:>25}'.format(index + 1, filename_list[index]))
-        
-    # create empty dictionary    
-    results_dic = dict()
+            # pet_labels.append(pet_name)
+                      
+            print('\n{:2d} file: {:>25}'.format(index + 1, filename_list[index]))
+   
+            # count length of empty dictionary
+            number_of_items_empty_dic = len(results_dic)
     
-    # count length of empty dictionary
-    number_of_items_empty_dic = len(results_dic)
-    print('\nEmpty dictionary has {} items'.format(number_of_items_empty_dic))
+            print('\nEmpty dictionary has {} items'.format(number_of_items_empty_dic))
     
-    # add every element of pet_labels as a value to the results_dic
-    for index in range (0, len(filename_list), 1):
-          if filename_list[index] not in results_dic:
-              results_dic[filename_list[index]] = [pet_labels[index]] # what does filenames refer to, here?
-          else:
-              print('\nWARNING: key =' , filenames[index],  'already exists in results_dic with value =', results_dic[filenames[index]])
+            # add every element of pet_labels as a value to the results_dic
+            if filename_list[index] not in results_dic:
+                results_dic[filename_list[index]] = [pet_name]# what does filenames refer to, here?
+            else:
+                print('\nWARNING: key =' , filenames[index],  'already exists in results_dic with value =', results_dic[filenames[index]])
           
     print('\nPrinting all key-value pairs in dictionary results_dic:')
     for key in results_dic:
